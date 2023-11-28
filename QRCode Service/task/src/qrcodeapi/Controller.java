b26ddca76e27a45e7f8ac17ec5bd6b6a9a626931
seamus52 <7493527+seamus52@users.noreply.github.com>
2023-11-28 07:04:14 -0800

    stage 5 complete

--- QRCode Service/task/src/qrcodeapi/Controller.java
+++ QRCode Service/task/src/qrcodeapi/Controller.java
@@ -1,5 +1,6 @@
 package qrcodeapi;
 
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -7,39 +8,49 @@ import org.springframework.web.bind.annotation.*;
 
 import java.awt.image.BufferedImage;
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
-    static final Map<String, MediaType> validImageFormats =
-            // TODO [lo-pri] convert to enum & enumset
-            Map.of("png", MediaType.IMAGE_PNG,
-                    "jpeg", MediaType.IMAGE_JPEG,
-                    "gif", MediaType.IMAGE_GIF);
+    private static final String CONTENT_EMPTY_ERROR_MSG = "Contents cannot be null or blank";
+    private static final String IMAGE_SIZE_INVALID_ERROR_MSG = "Image size must be between 150 and 350 pixels";
+    private static final String CORRECTION_LEVEL_INVALID_ERROR_MSG = "Permitted error correction levels are L, M, Q, H";
+    private static final String IMAGE_FORMAT_INVALID_ERROR_MSG = "Only png, jpeg and gif image types are supported";
+    private static final Map<String, MediaType> validImageFormats =
+            Map.of("PNG", MediaType.IMAGE_PNG,
+                    "JPEG", MediaType.IMAGE_JPEG,
+                    "GIF", MediaType.IMAGE_GIF);
+    private static final Set<String> validCorrectionValues = Set.of("L", "M", "Q", "H");
     @GetMapping("/health")
     public ResponseEntity<String> health() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/qrcode")
-    public ResponseEntity<?> getImage(@RequestParam String contents, @RequestParam int size, @RequestParam String type) {
-        // TODO [lo-pri] eliminate magic numbers and hard-coded messages
+    public ResponseEntity<?> getImage(@RequestParam String contents, @RequestParam(defaultValue = "250") int size,
+                                      @RequestParam(defaultValue = "PNG") String type,
+                                      @RequestParam(defaultValue = "L") String correction) {
         if (contents.isBlank() || contents.isEmpty()) {
-            return new ResponseEntity<>(Map.of("error","Contents cannot be null or blank"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(Map.of("error",CONTENT_EMPTY_ERROR_MSG), HttpStatus.BAD_REQUEST);
         }
 
         if (size < 150 || size > 350) {
-            return new ResponseEntity<>(Map.of("error","Image size must be between 150 and 350 pixels"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(Map.of("error",IMAGE_SIZE_INVALID_ERROR_MSG), HttpStatus.BAD_REQUEST);
         }
 
-        if (!validImageFormats.containsKey(type.toLowerCase())) {
-            return new ResponseEntity<>(Map.of("error","Only png, jpeg and gif image types are supported"), HttpStatus.BAD_REQUEST);
+        if (!validCorrectionValues.contains(correction.toUpperCase())) {
+            return new ResponseEntity<>(Map.of("error",CORRECTION_LEVEL_INVALID_ERROR_MSG), HttpStatus.BAD_REQUEST);
         }
 
-        BufferedImage bufferedImage = ImageSource.createImage(contents, size);
+         if (!validImageFormats.containsKey(type.toUpperCase())) {
+            return new ResponseEntity<>(Map.of("error",IMAGE_FORMAT_INVALID_ERROR_MSG), HttpStatus.BAD_REQUEST);
+        }
+
+        BufferedImage bufferedImage = ImageSource.createImage(contents, size, ErrorCorrectionLevel.valueOf(correction));
         return ResponseEntity
                 .ok()
-                .contentType(validImageFormats.get(type))
+                .contentType(validImageFormats.get(type.toUpperCase()))
                 .body(bufferedImage);
     }
 }
--- QRCode Service/task/src/qrcodeapi/ImageSource.java
+++ QRCode Service/task/src/qrcodeapi/ImageSource.java
@@ -1,24 +1,28 @@
 package qrcodeapi;
 
 import java.awt.image.BufferedImage;
+import java.util.Map;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.EncodeHintType;
 
 
 public class ImageSource {
 
-    public static BufferedImage createImage(String data, int size) {
+    public static BufferedImage createImage(String data, int size, ErrorCorrectionLevel errorCorrectionLevel) {
         BufferedImage bufferedImage = null;
         QRCodeWriter writer = new QRCodeWriter();
+        Map<EncodeHintType, ?> hints = Map.of(EncodeHintType.ERROR_CORRECTION, errorCorrectionLevel);
         try {
-            BitMatrix bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, size, size);
+            BitMatrix bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, size, size, hints);
             bufferedImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
         } catch (WriterException e) {
-            // TODO log and handle the WriterException
+            // TODO log and handle the WriterException properly
             throw new RuntimeException("Failed to generate QR code from string: " + data);
         }
 
